f53566ab5eafdd59ae4a357654a0eaf57a2d6aa3
RomanKamko <r.kamko@mail.ru>
2023-03-28 16:50:43 +0400

    не большие доработки, после самопроверки

--- controller/EmployeeController.java
+++ controller/EmployeeController.java
@@ -20,24 +20,24 @@ public class EmployeeController {
     }
 
     @GetMapping(path = "/add")
-    public Employee addEmployee(@RequestParam("firstName") String firstName,
-                                @RequestParam("lastName") String lastName) {
+    public Employee addEmployee(@RequestParam String firstName,
+                                @RequestParam String lastName) {
         return employeeService.add(firstName, lastName);
     }
 
     @GetMapping(path = "/remove")
-    public Employee rempveEmployee(@RequestParam("firstName") String firstName,
-                                   @RequestParam("lastName") String lastName) {
+    public Employee rempveEmployee(@RequestParam String firstName,
+                                   @RequestParam String lastName) {
         return employeeService.remove(firstName, lastName);
     }
 
     @GetMapping(path = "/find")
-    public Employee findEmployee(@RequestParam("firstName") String firstName,
-                                 @RequestParam("lastName") String lastName) {
+    public Employee findEmployee(@RequestParam String firstName,
+                                 @RequestParam String lastName) {
         return employeeService.find(firstName, lastName);
     }
 
-    @GetMapping(path = "/printEmployeeList")
+    @GetMapping
     public Collection<Employee> printEmployeeList() {
         return employeeService.printAll();
     }
